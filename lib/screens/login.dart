7f2e3e1c16186616bf38615822a438ae6eb614bb
Ganza <charlesganza@gmail.com>
2022-03-16 22:55:15 +0200

    changes

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -121,7 +121,7 @@ class _LoginPageState extends State<LoginPage> {
                       padding: EdgeInsets.all(10),
                       child: TextFormField(
                         controller: nameController,
-                        maxLength: 18,
+                        maxLength: 35,
                         validator: (text) {
                           if (text == null || text.isEmpty) {
                             return 'fillThisField'.tr();

24f1a799e6a3894f505dcfb92605cf42fac9d560
Ganza <charlesganza@gmail.com>
2022-04-02 01:56:54 +0200

    changes

--- lib/screens/sangiza_ubumenyi.dart
+++ lib/screens/sangiza_ubumenyi.dart
@@ -24,7 +24,7 @@ class SangizaUbumenyi extends StatelessWidget {
         Container(
           margin: EdgeInsets.only(top: 10),
           child: Text(
-            "Tanga Amakuru (Contact Us)",
+            "Sangiza Ubumenyi (Contact Us)",
             style: TextStyle(
                 color: Theme.of(context).primaryColor,
                 fontSize: 15,
@@ -66,90 +66,6 @@ class SangizaUbumenyi extends StatelessWidget {
             )
           ],
         ),
-        Divider(),
-        SizedBox(
-          height: 5,
-        ),
-        Padding(padding: EdgeInsets.all(10),
-          child: Column(
-            children: [
-              GestureDetector(
-                onTap: () {
-                  openEmail();
-                },
-                child: ContactItem(
-                  label: Strings.EMAIL,
-                  icon: Icons.email,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              GestureDetector(
-                onTap: () {
-                  launchURL('tel:+31620699903');
-                },
-                child: ContactItem(
-                  label: Strings.PHONE_NUMBER,
-                  icon: Icons.phone,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              GestureDetector(
-                onTap: () {
-                  launchURL("https://www.umuhuza-iwacu.org");
-                },
-                child: ContactItem(
-                  label: Strings.WEB_SITE,
-                  icon: FontAwesomeIcons.weebly,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              GestureDetector(
-                onTap: () {
-                  showAlertDialog(context, "whatsapp");
-                },
-                child: ContactItem(
-                  label: Strings.WHATSTAPP,
-                  icon: FontAwesomeIcons.whatsapp,
-                ),
-              ),
-              SizedBox(
-                height: 5,
-              ),
-              GestureDetector(
-                onTap: () {
-                  launchURL("https://facebook.com/umuhuza");
-                },
-                child: ContactItem(
-                  label: Strings.FACEBOOK,
-                  icon: Icons.facebook,
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  launchURL("https://instagram.com/umuhuza");
-                },
-                child: ContactItem(
-                  label: Strings.INSTAGRAM,
-                  icon: FontAwesomeIcons.instagram,
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  launchURL("https://twitter.com/umuhuza");
-                },
-                child: ContactItem(
-                  label: Strings.TWITER,
-                  icon: FontAwesomeIcons.twitter,
-                ),
-              ),
-            ],
-          ),)
       ],
     );
   }
